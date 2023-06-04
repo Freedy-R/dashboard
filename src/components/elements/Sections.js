fc74f1645d3956e2d7d8c60233bef4e68010b21b
Freedy-R <99390897+Freedy-R@users.noreply.github.com>
2023-06-04 11:21:54 +0200

    sections listState

--- src/components/elements/Sections.js
+++ src/components/elements/Sections.js
@@ -1,26 +1,37 @@
+import { useState } from "react";
 const Sections = () => {
-    return(
+    const [listState, setListState] = useState({
+        elements: [
+            { id: "12", title: "Dashboard", className: "btndashboard" },
+            { id: "22", title: "Insights", className: "btninsights" },
+            { id: "32", title: "Channels", className: "btnchannels" }
+        ]
+    })
+    const handleClick = (id) => {
+        console.log("Clicked element ID:", id);
+    }
+    return (
         <>
-         <section className="dashboard_sections">
-                    <section className="dashboard_section_nav">
-                        <label for="btnBack">
-                            <input className="btnBack" type="button" id="btnBack" value="<" />
-                            Back
-                        </label>
+            <section className="dashboard_sections">
+                <section className="dashboard_section_nav">
+                    <label for="btnBack">
+                        <input className="btnBack" type="button" id="btnBack" value="<" />
+                        Back
+                    </label>
+                    <ul>
                         <ul>
-                            <li>
-                            <button className="thing">Dashboard</button>
-                            </li>
-                            <li>
-                            <button className="thing">Insights</button>
-                            </li>
-                            <li>
-                            <button className="thing">Channels</button>
-                            </li>
+                            {listState.elements.map((element) => (
+                                <li key={element.id}>
+                                    <button className={element.className} onClick={() => handleClick(element.id)}>
+                                        {element.title}
+                                    </button>
+                                </li>
+                            ))}
                         </ul>
-                    </section>
-                    <section class="dashboard_sections_view"></section>
+                    </ul>
                 </section>
+                <section class="dashboard_sections_view"></section>
+            </section>
         </>
     )
 }
